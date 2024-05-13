7d122941c59ebcf4a5b10e5a523aceb741ada7ee
Riyanhidayat-222 <rrynhdya.444@gmail.com>
2024-05-14 03:58:15 +0700

    Update some word

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose')
+const logger = require('morgan')
 const Todos = require('./models/todos')
 
 app.use(bodyParser.json())
@@ -10,6 +11,13 @@ mongoose.connect('mongodb://localhost:27017/learn-api', {
     /*useNewUrlParser: true,
     useUnifiedTopology: true,*/
 })
+
+app.use(logger('dev'))
+
+app.get('/', (request, response) =>{
+    response.end('Hello rayyeayyen!')
+})
+
 
 app.get('/todo', (req, res) => {
     Todos.find({}).then((items) => {
